bf5fec92a3d93e48b51960729e122c336a379890
Nguyen Ba Khoa <khoa.nguyenba@outlook.com>
2025-10-23 01:15:57 +0700

    Add production ENV on build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const ENV = process.env.NODE_ENV;
 
-const server = ENV == "production" ? {} : {
+const server = ENV == "production" ? undefined : {
   url: "http://acer.mobile-devurl-greenmind.khoav4.com/", 
   // url: "http://mobile-devurl-greenmind.khoav4.com/",  // PC URL 
   cleartext: true 
